48762f0693a77194c44df3cc4e44fb0c276c3e7a
andychang <send2Andrew@gmail.com>
2013-02-20 15:12:47 +0000

    specflow - create album
    create album test

---
+++ SpecFlowTests/Features/Album.feature
@@ -0,0 +1,12 @@
+﻿Feature: Album
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Create an Album
+	Given a user is creating an album 
+	And the name is Spain
+	And the genre is Landscape
+	When the web request is made
+	Then the album will be sucessfully created
---
+++ SpecFlowTests/Steps/AlbumSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using AlbSvc = Photography.AlbumService;
+using DALDomain = Photography.NHibernateDAL.Domain;
+
+namespace SpecFlowTests.Steps
+{
+    [Binding]
+    public class AlbumSteps
+    {
+        AlbSvc.AlbumService albumService;
+        DALDomain.Album dalAlbum;
+
+        [Given(@"a user is creating an album")]
+        public void GivenAUserIsCreatingAnAlbum()
+        {
+            albumService = new AlbSvc.AlbumService();
+            dalAlbum = new DALDomain.Album();
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the name is (.*)")]
+        public void GivenTheNameIs(string newAlbumName)
+        {
+            dalAlbum.Name = newAlbumName;
+            dalAlbum.CreationDate = DateTime.Now;
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the genre is (.*)")]
+        public void GivenTheGenreIs(string newAlbumGenre)
+        {
+            dalAlbum.Genre = newAlbumGenre;
+            //ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the web request is made")]
+        public void WhenTheWebRequestIsMade()
+        {
+            albumService.CreateAlbum(dalAlbum);
+           // ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the album will be sucessfully created")]
+        public void ThenTheAlbumWillBeSucessfullyCreated_()
+        {
+            //albumService.SearchAlbumByName()
+            //ScenarioContext.Current.Pending();
+        }
+    }
+}
